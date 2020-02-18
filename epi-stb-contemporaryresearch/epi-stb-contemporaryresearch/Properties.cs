0eb7f817bed457766b0b50bb6b288e2e62be1dc0
Trevor Payne <tpayne@pepperdash.com>
2020-02-18 10:17:05 -0600

    initial commit

---
+++ epi-stb-contemporaryresearch/epi-stb-contemporaryresearch/Properties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+using PepperDash.Core;
+using PepperDash.Essentials.Core;
+
+namespace epi_stb_contemporaryresearch
+{
+    public class Properties
+    {
+        public ControlPropertiesConfig Control { get; set; }
+
+        public string unitId { get; set; }
+    }
+}
---
+++ epi-stb-contemporaryresearch/epi-stb-contemporaryresearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("epi_stb_contemporaryresearch")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("epi_stb_contemporaryresearch")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
