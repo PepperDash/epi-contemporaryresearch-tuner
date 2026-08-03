75044054485edf1afadb3d7d748294f126ec004e
equinoy <equinoy@pepperdash.com>
2026-08-03 16:32:12 -0400

    fix(contemporaryresearch): add CommFactory compatibility call for Essentials runtime mismatch

--- src/ContemporaryResearchFactory.cs
+++ src/ContemporaryResearchFactory.cs
@@ -1,7 +1,9 @@
 ﻿using PepperDash.Essentials.Core;
 using PepperDash.Essentials.Core.Config;
 using PepperDash.Core;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace epi_stb_contemporaryresearch
 {
@@ -18,7 +20,7 @@ namespace epi_stb_contemporaryresearch
         {
             Debug.Console(1, "Factory Attempting to create new Contemporary Research device");
 
-            var comms = CommFactory.CreateCommForDevice(dc);
+            var comms = CreateCommForDeviceCompat(dc);
             var device = new ContemporaryResearchDevice(dc.Key, dc.Name, comms, dc);
 
             var listName = dc.Properties.Value<string>("presetsList");
@@ -26,6 +28,24 @@ namespace epi_stb_contemporaryresearch
                 device.LoadPresets(listName);
 
             return device;
+        }
+
+        private static IBasicCommunication CreateCommForDeviceCompat(DeviceConfig dc)
+        {
+            var method = typeof(CommFactory).GetMethod("CreateCommForDevice", BindingFlags.Public | BindingFlags.Static, null, new[] { typeof(DeviceConfig) }, null);
+            if (method == null)
+            {
+                throw new MissingMethodException("CommFactory.CreateCommForDevice(DeviceConfig) was not found.");
+            }
+
+            var commObj = method.Invoke(null, new object[] { dc });
+            var comm = commObj as IBasicCommunication;
+            if (commObj != null && comm == null)
+            {
+                throw new InvalidCastException(string.Format("CommFactory.CreateCommForDevice returned '{0}', which does not implement IBasicCommunication.", commObj.GetType().FullName));
+            }
+
+            return comm;
         }
     }
 }
